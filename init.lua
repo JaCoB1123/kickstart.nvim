f9c5366e353cb1111a954ef67e3c58e454e9dfc1
Jan Bader <c.github@jan.javil.eu>
2024-11-27 00:20:46 +0100

    nvim: add code to handle tmux sessions

--- init.lua
+++ init.lua
@@ -64,6 +64,22 @@ vim.g.clipboard = {
         ["*"] = require("vim.ui.clipboard.osc52").paste "*",
     },
 }
+if vim.env.TMUX ~= nil then
+  local copy = {'tmux', 'load-buffer', '-w', '-'}
+  local paste = {'bash', '-c', 'tmux refresh-client -l && sleep 0.05 && tmux save-buffer -'}
+  vim.g.clipboard = {
+    name = 'tmux',
+    copy = {
+      ['+'] = copy,
+      ['*'] = copy,
+    },
+    paste = {
+      ['+'] = paste,
+      ['*'] = paste,
+    },
+    cache_enabled = 0,
+  }
+end
 
 -- Enable break indent
 vim.opt.breakindent = true
