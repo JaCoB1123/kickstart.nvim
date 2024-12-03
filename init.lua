d8cba1f30d8aabfd9686d0831312bfacf1837ce2
Jan Bader <c.github@jan.javil.eu>
2024-12-04 00:36:38 +0100

    nvim: do not override build for native fzf

--- init.lua
+++ init.lua
@@ -257,10 +257,10 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
+        --build = 'make',
+        --cond = function()
+        --  return vim.fn.executable 'make' == 1
+        --end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
