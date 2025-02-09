5e4f83ae96235848a10e4efffc2e2a785c813a82
Jan Bader <c.github@jan.javil.eu>
2025-02-09 19:49:27 +0100

    nvim: add avante providers to blink

--- init.lua
+++ init.lua
@@ -960,6 +960,24 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
             score_offset = 3,
             kind = 'supermaven',
           },
+          avante_commands = {
+            name = "avante_commands",
+            module = "blink.compat.source",
+            score_offset = 90, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_files = {
+            name = "avante_files",
+            module = "blink.compat.source",
+            score_offset = 100, -- show at a higher priority than lsp
+            opts = {},
+          },
+          avante_mentions = {
+            name = "avante_mentions",
+            module = "blink.compat.source",
+            score_offset = 1000, -- show at a higher priority than lsp
+            opts = {},
+          }
           --    'saadparwaiz1/cmp_luasnip',
 
           -- Adds other completion capabilities.
