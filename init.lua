a4912e8e404703c744b9acf0eca42ed46f8eceb1
Jan Bader <c.github@jan.javil.eu>
2025-02-12 21:28:16 +0100

    nvim: disable avantes auto_suggestions
    use supermaven instead

--- init.lua
+++ init.lua
@@ -1263,7 +1263,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       auto_suggestions_provider = 'openai',
       provider = 'openai',
       behaviour = {
-        auto_suggestions = true,
+        auto_suggestions = false,
         auto_set_highlight_group = true,
         auto_set_keymaps = true,
         auto_apply_diff_after_generation = false,
